5cd1189fa557c8ae53bc2a906af89e2eeb1a9dba
enmarcm <theenmanuel123@gmail.com>
2024-06-26 13:32:04 -0400

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import songsMocks from "../../mocks/music.json"
+import { ItemCardComponent } from '../item-card/item-card.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [ CommonModule, FormsModule, IonicModule]
+  imports: [ CommonModule, FormsModule, IonicModule, ItemCardComponent]
 })
 export class HomePage implements OnInit {
 
   constructor() { }
 
+  public songs: any[] = songsMocks
+
   ngOnInit() {
+
   }
 
 }
--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-item-card',
+  selector: 'item-card',
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
 })
-export class ItemCardComponent  implements OnInit {
-
-  constructor() { }
+export class ItemCardComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
 
+  @Input() songName: string = 'cancion';
+  @Input() artistName: string[] = ['artista'];
+  @Input() urlImage: string =
+    'https://ionicframework.com/docs/img/demos/card-media.png';
+  @Input() idSong: string = 'id';
 }

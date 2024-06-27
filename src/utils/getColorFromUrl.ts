f7fc8256fba78442bcfca9060d2e142b9599b7f9
enmarcm <theenmanuel123@gmail.com>
2024-06-27 12:00:55 -0400

    changes

--- src/utils/getColorFromUrl.ts
+++ src/utils/getColorFromUrl.ts
@@ -4,25 +4,36 @@ async function getDominantColorHex(imageUrl: string): Promise<string> {
     img.crossOrigin = 'Anonymous';
     img.src = imageUrl;
 
-    await new Promise((resolve) => {
+    await new Promise((resolve, reject) => {
       img.onload = resolve;
-      img.onerror = resolve;
+      img.onerror = reject;
     });
 
+    const sampleSize = 10; // Tamaño del área de muestreo (10x10)
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    canvas.width = 1;
-    canvas.height = 1;
+    canvas.width = sampleSize;
+    canvas.height = sampleSize;
 
     if (!context) return '';
-    context.drawImage(img, 0, 0, 1, 1);
+    context.drawImage(img, 0, 0, sampleSize, sampleSize);
 
-    const imageData = context.getImageData(0, 0, 1, 1).data;
-    const rgb = { r: imageData[0], g: imageData[1], b: imageData[2] };
+    const imageData = context.getImageData(0, 0, sampleSize, sampleSize).data;
+    let r = 0, g = 0, b = 0;
+    const totalPixels = imageData.length / 4; // Cada pixel tiene 4 componentes (RGBA)
 
-    const color = rgbToHex(rgb.r, rgb.g, rgb.b);
+    for (let i = 0; i < imageData.length; i += 4) {
+      r += imageData[i];
+      g += imageData[i + 1];
+      b += imageData[i + 2];
+    }
 
-    if (!color) return '';
+    // Calcula el promedio de cada componente de color
+    r = Math.floor(r / totalPixels);
+    g = Math.floor(g / totalPixels);
+    b = Math.floor(b / totalPixels);
+
+    const color = rgbToHex(r, g, b);
 
     return color;
   } catch (error) {
@@ -31,11 +42,11 @@ async function getDominantColorHex(imageUrl: string): Promise<string> {
   }
 }
 
-function rgbToHex(r: any, g: any, b: any) {
+function rgbToHex(r: number, g: number, b: number): string {
   return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 }
 
-function componentToHex(c: any) {
+function componentToHex(c: number): string {
   const hex = c.toString(16);
   return hex.length === 1 ? '0' + hex : hex;
 }
